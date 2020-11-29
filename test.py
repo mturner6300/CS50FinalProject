dd087bd1bd3890746bc9071088f03f25f81df525
Meghan Turner <mturner6300@gmail.com>
2020-11-28 22:14:12 -0500

    Linking more jinja pages

--- test.py
+++ test.py
@@ -3,5 +3,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    home = "currentpage"
+    return render_template("index.html", home=home)
+
+@app.route("/about", methods=(["GET","POST"]))
+def about():
+    about = "currentpage"
+    return render_template("about.html", about=about)
+
+@app.route("/explore", methods=(["GET","POST"]))
+def explore():
+    explore = "currentpage"
+    return render_template("explore.html", explore=explore)
     
