8a028247995dd2b6586beb29fa39405df4056ce5
Meghan Turner <mturner6300@gmail.com>
2020-11-29 21:19:04 -0500

    installed flask_session and required login pages

--- app.py
+++ app.py
@@ -1,5 +1,23 @@
 from flask import Flask, flash, redirect, render_template, request, session
+from helpers import login_required
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
+
 app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
 
 import sqlite3
 connection = sqlite3.connect("coursedatabase.db")
@@ -10,22 +28,29 @@ def index():
     home = "currentpage"
     return render_template("index.html", home=home)
 
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    return render_template("login.html")
+
 @app.route("/about", methods=(["GET","POST"]))
 def about():
     about = "currentpage"
     return render_template("about.html", about=about)
 
 @app.route("/explore", methods=(["GET","POST"]))
+@login_required
 def explore():
     explore = "currentpage"
     return render_template("explore.html", explore=explore)
 
 @app.route("/tracks", methods=(["GET","POST"]))
+@login_required
 def tracks():
     tracks = "currentpage"
     return render_template("tracks.html", tracks=tracks)
 
 @app.route("/myaccount", methods=(["GET","POST"]))
+@login_required
 def myaccount():
     myaccount = "currentpage"
     return render_template("myaccount.html", myaccount=myaccount)
