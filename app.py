b15ee9497090477789ab378ab9a2e967132e869f
sblack20 <samblackman20@gmail.com>
2020-11-30 00:06:50 -0500

    Added /register route and page

--- app.py
+++ app.py
@@ -35,6 +35,10 @@ def index():
 def login():
     return render_template("login.html")
 
+@app.route('/register', methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
+
 """ Combine About and Contact Us?"""
 @app.route("/about", methods=(["GET","POST"]))
 def about():
