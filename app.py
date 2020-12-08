cf3be25d17c834f75d30076aa237194bfe79473b
Meghan Turner <mturner6300@gmail.com>
2020-12-07 22:07:08 -0500

    Comments

--- app.py
+++ app.py
@@ -27,7 +27,6 @@ def make_cursor(database):
 # Global constant to limit search results to 25 per page
 perpage = 25
 
-
 # Ensure responses aren't cached
 @app.after_request
 def after_request(response):
@@ -36,19 +35,21 @@ def after_request(response):
     response.headers["Pragma"] = "no-cache"
     return response
 
+# Redirects to the homepage and highlights tab
 @app.route('/')
 def index():
     home = "currentpage"
     return render_template("index.html", home=home)
 
+# Redirects to the login page
 @app.route('/login', methods=["GET", "POST"])
 def login():
     # Forget any user ids
     session.clear()
     # Enter database
     conn, db = make_cursor("coursedatabase.db")
-
     login = "currentpage"
+
     # Post is submission
     if request.method == "POST":
         # Ensure username and password were submitted
@@ -67,16 +68,18 @@ def login():
         if not check_password_hash(hashed_pass,request.form.get("password")):
             return render_template("login.html", message="Wrong password", login=login)
         
-
         # Store username and return to home
         session["user_id"] = user_id
         return redirect("/")
+
     else:
         return render_template("login.html", login=login)
 
-
+# Redirects to the registration page
 @app.route('/register', methods=["GET", "POST"])
 def register():
+
+    # If method is POST, connect to database and prepare security questions in case of redirection
     if request.method == "POST":
         conn, db = make_cursor("coursedatabase.db")
         db.execute("SELECT * FROM security")
@@ -91,42 +94,50 @@ def register():
         security_answer = request.form.get("security_answer")
         confirm_answer = request.form.get("confirm_answer")
 
+        # Return an error if there is a missing field
         if not username or not password or not confirm_pass or not email or not security_id or not security_answer or not confirm_answer:
             error = "Please fill in all fields!"
             return render_template("register.html", error=error, questions=questions)
+
+        # Return an error if the password does not match the confirm password field
         elif password != confirm_pass:
-            #return passwords don't match error
             error = "Passwords do not match!"
-            return render_template("register.html", error=error,questions=questions)
+            return render_template("register.html", error=error, questions=questions)
+
+        # Return an error if the security answer does not match the confirm 
         elif security_answer != confirm_answer:
-            #return security answers don't match error
             error = "Security answers do not match!"
-            return render_template("register.html", error=error,questions=questions)
+            return render_template("register.html", error=error, questions=questions)
+
+        # Return an error if username is not 
         elif len(db.execute("SELECT * FROM users WHERE username = ?", [request.form.get("username")]).fetchall()) != 0:
-            #return username taken error
             error = "Username is taken!"
             return render_template("register.html", error=error, questions=questions)
+
+        # Return an error if email already has an account associated with it
         elif len(db.execute("SELECT * FROM users WHERE email = ?", [request.form.get("email")]).fetchall()) != 0:
-            #return email error
             error = "Email already has an account!"
             return render_template("register.html", error=error, questions=questions)
 
+        # If all conditions are met, let user know registration is complete and store their data
         else:
             error = "Registration Successful!"
             db.execute("INSERT INTO users (username, email, hashedpass, security_id, security_hash) VALUES (?, ?, ?, ?, ?)", (username, email, generate_password_hash(password),security_id, generate_password_hash(security_answer)))
             conn.commit()
             return render_template("register.html", error=error, questions=questions)
 
-
+    # If method is GET, connect to database and load form with security questions
     else:
         conn, db = make_cursor("coursedatabase.db")
         db.execute("SELECT * FROM security")
         questions = db.fetchall()
         return render_template("register.html", questions=questions)
 
-""" Combine About and Contact Us?"""
-@app.route("/about", methods=(["GET","POST"]))
+""" About / Contact Us """
+# Redirects to the about / contact us page
+@app.route("/about")
 def about():
+    # Highlights the about page tab
     about = "currentpage"
     return render_template("about.html", about=about)
 
@@ -138,36 +149,48 @@ def tracks():
     return render_template("tracks.html", tracks=tracks)
 
 """ Course Search"""
+# Redirects to the course search page
 @app.route("/search")
 def search():
+    # Highlights the course search tab in the navigation bar
     search = "currentpage"
     return render_template("search.html", search=search)
 
 
 """ Course Search Results """
+# Loads results from course search
 @app.route("/searchresults", methods=(["GET","POST"]))
 def searchresults():
+
+    # Connects to database and stores user's search in variable and cookie
+    # Stores current page of results in a cookie for later and creates an offset for later
     conn, db = make_cursor("coursedatabase.db")
     search = "currentpage"
     querystring = request.args.get("q")
+    session["last_search"] = querystring
     page = request.args.get(get_page_parameter(), type=int, default=1) 
     session["page"] = page
     offset = (page - 1) * perpage
-
-    session["last_search"] = querystring
+    
+    # If nothing was searched for, load all courses and paginate the results at 25 per page
     if not querystring:
         db.execute("SELECT * FROM courses LIMIT ? OFFSET ?", (perpage, offset))
         results = db.fetchall()
         total = db.execute("SELECT * FROM courses").fetchall()
         pagination = Pagination(page=page, total=len(total), search=False, record_name='courses', per_page=perpage, css_framework='bootstrap4')
         return render_template("results.html", querystring=querystring, search=search, results=results, pagination=pagination)
-                
+
+    # Otherwise, query for courses where the user's search is in the course name or course code           
     else:
         db.execute("SELECT * FROM courses WHERE INSTR(LOWER(name),LOWER(?)) OR INSTR(LOWER(code),LOWER(?))", (querystring, querystring))
         results = db.fetchall()
         total = results
+
+        # If no results come back, redirect to search page which will display an error
         if not results:
             return render_template("search.html", querystring=querystring, search=search)
+        
+        # If there are results for the search, paginate and display them
         else:
             db.execute("SELECT * FROM courses WHERE INSTR(LOWER(name),LOWER(?)) OR INSTR(LOWER(code),LOWER(?)) LIMIT ? OFFSET ?", (querystring, querystring, perpage, offset))
             results = db.fetchall()
@@ -175,23 +198,31 @@ def searchresults():
             return render_template("results.html", querystring=querystring, search=search, results=results, pagination=pagination)
         
 """ My Courses """
+
+# Redirects to the my courses page
 @app.route('/mycourses')
 @login_required
 def mycourses():
+    # Highlights navigaton bar tab, connects to the database, and stores user_id in a cookie
     mycourses = "currentpage"
     conn, db = make_cursor("coursedatabase.db")
     user_id = session["user_id"]
+
+    # Load the user's favourite and completed courses
     db.execute("SELECT courses.id, courses.name, courses.description, courses.code FROM courses JOIN favourites ON favourites.course_id = courses.id WHERE favourites.user_id = ?", [user_id])
     favourites = db.fetchall()
     completed = db.execute("SELECT courses.id, courses.name, courses.description, courses.code FROM courses JOIN completed ON completed.course_id = courses.id WHERE completed.user_id = ?", [user_id]).fetchall()
     return render_template("mycourses.html", mycourses=mycourses, favourites=favourites, completed=completed)
 
 
+# Executes this code when a course is favourited
 @app.route("/favourite", methods=(["GET","POST"]))
 @login_required
 def favourite():
+    # Stores page number from the cookie we created in searchresults
     pagenum = session["page"]
     mycourses = "currentpage"
+
     if request.method == "GET":
         conn, db = make_cursor("coursedatabase.db")
         user_id = session["user_id"]
